f52d47b4475c650089d9698a90d9bc1d53998ba5
Anvi Dobhal <dobhala28@mcmsnj.net>
2026-06-07 13:59:54 -0400

    Initial Commit

---
+++ Note Hop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Note Hop
+//
+//  Created by Anvi Dobhal on 6/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note Hop/Note_HopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_HopApp.swift
+//  Note Hop
+//
+//  Created by Anvi Dobhal on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Note_HopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
