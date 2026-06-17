955215c6f8bef37fd69f0f4a91409e09053fc534
Anvi Dobhal <dobhala28@mcmsnj.net>
2026-06-17 16:39:26 -0400

    Added app icon for Note Hop! Then, changed string literals in the set of tabs to variables.

---
+++ Note Hop/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Note Hop
+//
+//  Created by Anvi Dobhal on 6/17/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
