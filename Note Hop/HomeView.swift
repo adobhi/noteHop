0e968c77df70a06835544c25549850b236c4e45b
Anvi Dobhal <dobhala28@mcmsnj.net>
2026-07-07 22:55:51 -0400

    Added another sheet music image, added function for sheet music button formatting, removed padding from button, added header banner to homepage to stick in scroll feature.

--- Note Hop/HomeView.swift
+++ Note Hop/HomeView.swift
@@ -9,134 +9,97 @@ import SwiftUI
 
 struct HomeView: View {
     var noteImage = Constants.noteImageURL
-    var sheetMusicImage = Constants.sheetMusicImageURL
+    var sheetMusicImage1 = Constants.sheetMusicImageURL1
+    var sheetMusicImage2 = Constants.sheetMusicImageURL2
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                Spacer()
-                
-                let greetings = ["Look who finally decided to show up", "Oh, great, you’re back. The data missed you", "Look out, the main character has logged in", "Your data has been expecting you, Captain", "Warning: High levels of productivity ahead", "My algorithms predict you will do great things", "Loading your digital empire", "The data is ready. Are you?"]
-                let randomInt = Int.random(in: 0 ..< greetings.count-1)
-                let greeting = greetings[randomInt]
-                Text(greeting)
-                    .font(.custom("DynaPuff-Regular", size: 45))
-                
+            VStack(alignment: .leading, spacing: 15) {
                 Spacer()
                 
                 Text("Ready for your next lesson?")
-                    .cuteFont(30, 10)
+                    .cuteFont(30)
+                    .padding(.leading, 20)
                 HStack {
                     Button {
                         
                     } label: {
                         Text(Constants.firstLesson)
-                            .cuteFont(20,5)
+                            .cuteFont(20)
                             .pinkButton()
                     }
+                    .padding(.leading,20)
                     Button {
                         
                     } label: {
                         Text(Constants.secondLesson)
-                            .cuteFont(20,5)
+                            .cuteFont(20)
                             .pinkButton()
                     }
+
                 }
                 
                 Spacer()
                 
                 Text("Pick up where you left off?")
-                    .cuteFont(30,10)
+                    .cuteFont(30)
+                    //.padding(.leading,20)
                 HStack (){
                     Button {
                         
                     } label: {
-                        AsyncImage(url: URL(string: sheetMusicImage)) { image in
-                            image
-                            //.resizable()
-                                .scaledToFill()
-                            //.clipped()
-                                .frame(width:100, height: 100)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .stroke(Color("buttonColor"), lineWidth: 5)
-                                )
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        /*
-                         Text(Constants.music1)
-                         .cuteFont(15,5)
-                         .pinkButton()
-                         
-                         .frame(width: 100, height: 100)
-                         .foregroundStyle(Color("textColor"))
-                         //.bold()
-                         .background{
-                         RoundedRectangle(cornerRadius: 20, style: .continuous)
-                         .stroke(Color("buttonColor"),lineWidth:5)
-                         
-                         }
-                         */
+                        sheetMusicButton(sheetMusicImage1)
                     }
                     
                     Button {
                         
                     } label: {
-                        AsyncImage(url: URL(string: noteImage)) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width:100, height: 100)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 20)
-                                        .stroke(Color("buttonColor"), lineWidth: 5)
-                                )
-                        } placeholder: {
-                            ProgressView()
-                        }
+                        sheetMusicButton(sheetMusicImage2)
                     }
                 }
                 
-                
                 Spacer()
                 
-                Text("Pick up where you left off?")
-                    .cuteFont(30,10)
+                Text("Let's see how you're doing?")
+                    .cuteFont(30)
+
                 HStack (){
                     Button {
                         
                     } label: {
                         Text(Constants.music1)
-                            .cuteFont(20,5)
-                            .frame(width: 100, height: 100)
-                            .foregroundStyle(Color("textColor"))
-                        //.bold()
-                            .background{
-                                RoundedRectangle(cornerRadius: 20, style: .continuous)
-                                    .stroke(Color("buttonColor"),lineWidth:5)
+                            .cuteFont(20)
+                            .pinkButton()
                                 
-                            }
                     }
                     Button {
                         
                     } label: {
                         Text(Constants.secondLesson)
-                            .cuteFont(20,5)
+                            .cuteFont(20)
                             .pinkButton()
                     }
                 }
                 
                 Spacer()
-                
                 
                 
             }
             .frame(maxWidth: .infinity)
 
         }
+        .safeAreaInset(edge: .top, spacing: 0) {
+            let greetings = ["Look who finally decided to show up", "Oh, great, you’re back. The data missed you", "Look out, the main character has logged in", "Your data has been expecting you, Captain", "Warning: High levels of productivity ahead", "My algorithms predict you will do great things", "Loading your digital empire", "The data is ready. Are you?"]
+            let randomInt = Int.random(in: 0 ..< greetings.count-1)
+            let greeting = greetings[randomInt]
+            Text(greeting)
+                .cuteFont(40)
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color("buttonColor")
+                    .ignoresSafeArea(edges: .top))
+        }
+        
     }
 }
 
