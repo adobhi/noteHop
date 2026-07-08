52ca13eed176f883215349e2af72f38e93a85775
Anvi Dobhal <dobhala28@mcmsnj.net>
2026-07-07 22:55:51 -0400

    Added another sheet music image, added function for sheet music button formatting, removed padding from button, added header banner to homepage to stick in scroll feature.

--- Note Hop/Constants.swift
+++ Note Hop/Constants.swift
@@ -27,7 +27,8 @@ struct Constants {
     static let color3 = "FAFFAD"
     
     static let noteImageURL = "https://t3.ftcdn.net/jpg/05/85/10/62/360_F_585106274_GbJNWuJ9gnj93G19sRT4eK54ojKysO0t.jpg"
-    static let sheetMusicImageURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnXTJJ2JNIGfYwld9ET-ebuGM_uD-ZRiJW0iL-tR6mSA&s=10"
+    static let sheetMusicImageURL1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnXTJJ2JNIGfYwld9ET-ebuGM_uD-ZRiJW0iL-tR6mSA&s=10"
+    static let sheetMusicImageURL2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO-jsaOyyX1-GTceGJHFx-1Gwzy66nhR6K5Feb_zbYkA&s=10"
     static let music1 = "Can You Hear the Music"
     static let music2 = "Despacito"
     static let music3 = "Golden Hour"
@@ -46,10 +47,30 @@ extension View {
     }
 }
 
+extension View {
+    func sheetMusicButton(_ music: String) -> some View{
+        AsyncImage(url: URL(string: music)) { image in
+            image
+            //.resizable()
+                .scaledToFill()
+                //.padding(.horizontal, pad)
+            //.clipped()
+                .frame(width:100, height: 150)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color("buttonColor"), lineWidth: 5)
+                )
+        } placeholder: {
+            ProgressView()
+        }
+    }
+}
+
 extension Text {
-    func cuteFont(_ num: Double, _ pad: Double) -> some View{
+    func cuteFont(_ num: Double) -> some View{
         self
             .font(.custom("DynaPuff-Regular", size: num))
-            .padding(.horizontal, pad)
+            //.padding(.horizontal, pad)
     }
 }
