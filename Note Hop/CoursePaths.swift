c16bd881f8dbedc9ede76217dd54cf9caf59898f
Anvi Dobhal <dobhala28@mcmsnj.net>
2026-07-25 23:40:56 -0400

    Added optional parameters to piano, trebleInstruments, and bassInstruments so that they could shrink the name of the instrument and move the x coordinate of instrument image!

--- Note Hop/CoursePaths.swift
+++ Note Hop/CoursePaths.swift
@@ -1,4 +1,3 @@
-//
 //  CoursePaths.swift
 //  Note Hop
 //
@@ -9,190 +8,191 @@ import SwiftUI
 
 struct CoursePaths: View {
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("Instrument Courses")
-                    .cuteFont(30)
-                    .padding(.leading, 20)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyAqua"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Violin")
-                                    .cuteFont(25)
-                                    .foregroundStyle(.black)
-                                Image("violin")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 120, height: 80)
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    Text("Instrument Courses")
+                        .cuteFont(30)
+                        .padding(.leading, 20)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    HStack {
+                        NavigationLink {
+                            TrebleInstruments(title: "Violin",instrumentImage: "violin")
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyAqua"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Violin")
+                                        .cuteFont(25)
+                                        .foregroundStyle(.black)
+                                    Image("violin")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 120, height: 80)
+                                }
                             }
                         }
-                    }
-                    
-                    Button {
                         
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyYellow"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Piano")
-                                    .foregroundStyle(.black)
-                                    .cuteFont(25)
-                                Image("piano")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 80)
-                            }
-                        }
-                    }
-                    
-                }
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyPink"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Flute")
-                                    .foregroundStyle(.black)
-                                    .cuteFont(25)
-                                Image("flute")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 80)
-                            }
-                        }
-                    }
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyAqua"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Saxaphone")
-                                    .cuteFont(25)
-                                    .foregroundStyle(.black)
-                                Image("sax")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 80, height: 80)
+                        NavigationLink {
+                            Piano()
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyYellow"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Piano")
+                                        .foregroundStyle(.black)
+                                        .cuteFont(25)
+                                    Image("piano")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 80)
+                                }
                             }
                         }
                         
                     }
-                    
-                }
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyYellow"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Trumpet")
-                                    .foregroundStyle(.black)
-                                    .cuteFont(25)
-                                Image("trumpet")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 120, height: 80)
+                    HStack {
+                        NavigationLink {
+                            TrebleInstruments(title: "Flute",instrumentImage: "flute")
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyPink"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Flute")
+                                        .foregroundStyle(.black)
+                                        .cuteFont(25)
+                                    Image("flute")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 80)
+                                }
                             }
                         }
-                    }
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyPink"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Clarinet")
-                                    .cuteFont(25)
-                                    .foregroundStyle(.black)
-                                Image("clarinet")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 80, height: 80)
+                        NavigationLink {
+                            TrebleInstruments(title: "Alto Saxophone",instrumentImage: "sax", nameSize: 35)
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyAqua"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Alto Saxaphone")
+                                        .cuteFont(24)
+                                        .foregroundStyle(.black)
+                                        .frame(width: 150,height: 100)
+                                        //.padding(.bottom,20)
+                                    Image("sax")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 80, height: 80)
+                                        .offset(y:-20)
+
+                                }
                             }
-                        }
-                    }
-                    
-                    
-                }
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyAqua"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            VStack {
-                                Text("Cello")
-                                    .cuteFont(25)
-                                    .foregroundStyle(.black)
-                                Image("cello")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 120, height: 90)
-                            }
-                        }
-                    }
-                    
-                    Button {
-                        
-                    } label: {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(Color("heavyYellow"))
-                                .frame(width: 150, height: 150)
-                                .padding(15)
-                            Text("More Coming Soon!")
-                                .foregroundStyle(.black)
-                                .cuteFont(30)
-                                .frame(width: 120, height: 120)
                             
                         }
+                        
+                    }
+                    HStack {
+                        NavigationLink {
+                            TrebleInstruments(title: "Trumpet",instrumentImage: "trumpet")
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyYellow"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Trumpet")
+                                        .foregroundStyle(.black)
+                                        .cuteFont(25)
+                                    Image("trumpet")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 120, height: 80)
+                                }
+                            }
+                        }
+                        NavigationLink {
+                            TrebleInstruments(title: "Clarinet",instrumentImage: "clarinet")
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyPink"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Clarinet")
+                                        .cuteFont(25)
+                                        .foregroundStyle(.black)
+                                    Image("clarinet")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 80, height: 80)
+                                }
+                            }
+                        }
+                        
+                        
+                    }
+                    HStack {
+                        NavigationLink {
+                            BassInstruments(title: "Cello",instrumentImage: "cello")
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyAqua"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                VStack {
+                                    Text("Cello")
+                                        .cuteFont(25)
+                                        .foregroundStyle(.black)
+                                    Image("cello")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 120, height: 90)
+                                }
+                            }
+                        }
+                        
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color("heavyYellow"))
+                                    .frame(width: 150, height: 150)
+                                    .padding(15)
+                                Text("More Coming Soon!")
+                                    .foregroundStyle(.black)
+                                    .cuteFont(30)
+                                    .frame(width: 120, height: 120)
+                        }
+                        
                     }
                     
                 }
-                
+            }
+            .padding(.top,20)
+            .safeAreaInset(edge: .top, spacing: 0) {
+                Text("Course Paths")
+                    .cuteFont(50)
+                    .foregroundStyle(Color("bannerFont"))
+                    .padding(20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color("lightColor")
+                        .ignoresSafeArea(edges: .top))
+
             }
         }
-        .padding(.top,20)
-        .safeAreaInset(edge: .top, spacing: 0) {
-            Text("Course Paths")
-                .cuteFont(50)
-                .foregroundStyle(Color("bannerFont"))
-                .padding(20)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color("lightColor")
-                    .ignoresSafeArea(edges: .top))
-        }
-            
         }
     }
 
