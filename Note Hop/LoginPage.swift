0a7c851da01a441e3efff5a5f38e4c747b56299c
Anvi Dobhal <dobhala28@mcmsnj.net>
2026-07-28 05:43:48 -0400

    Guess what!!! Every change i made to login page got deleted when I tried pushing so this is the START of the rewrite for login page code!

--- Note Hop/LoginPage.swift
+++ Note Hop/LoginPage.swift
@@ -9,27 +9,32 @@ import SwiftUI
 
 struct LoginPage: View {
     var body: some View {
-        VStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: 0)
-                    .foregroundColor(Color("lightAqua"))
-                VStack {
-                    Text("Note Hop")
-                        .cuteFont(70)
-                        .bold()
-                        .padding(.bottom, 20)
-                    Text("Learn music fast, for fun, for and free!")
-                        .cuteFont(30)
-                        .frame(width: 300)
-                        .multilineTextAlignment(.center)
+        NavigationStack{
+            VStack {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 0)
+                        .foregroundColor(Color("lightAqua"))
+                        .ignoresSafeArea()
+                    VStack {
+                        Text("Note Hop")
+                            .cuteFont(70)
+                            .bold()
+                            .padding(.bottom, 20)
+                        Text("Learn music fast, for fun, for and free!")
+                            .cuteFont(30)
+                            .frame(width: 300)
+                            .multilineTextAlignment(.center)
+                    }
                 }
+                .frame(maxWidth: .infinity * 3)
+                .frame(height: 300)
             }
-            .frame(maxWidth: .infinity * 3)
-            .frame(height: 300)
+            Spacer()
         }
-        Spacer()
     }
 }
+
+
 
 #Preview {
     LoginPage()
